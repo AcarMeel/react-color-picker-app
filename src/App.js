1479c39b76b17d257b77627f6b3f222479f37a5d
acarmeel <acarmeel@MacBook-Pro-de-acarmeel.local>
2021-01-04 22:04:00 -0600

    Adding routes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import { generatePalette } from "./colorHelpers";
 import Palette from "./Palette";
+import PaletteList from "./PaletteList";
 import seedColors from "./seedColors";
 
 export default class App extends Component {
+  findPalette(id) {
+    return seedColors.find(palette => palette.id === id);
+  }
+
   render() {
     return (
       <Switch>
-        <Route exact path='/' />
-        <Route path='/palette/:id' />
+        <Route exact path='/' render={() => <PaletteList palettes={seedColors} /> } />
+        <Route path='/palette/:id' 
+          render={routeProps => (
+            <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
+          )}
+        />
       </Switch>
-      // <div>
-      //   <Palette palette={generatePalette(seedColors[4])} />
-      // </div>
     );
   }
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Slider from "rc-slider";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
@@ -39,7 +40,7 @@ export default class Navbar extends Component {
     return (
       <header className="Navbar">
         <div className="logo">
-          <a href="#">ReactColorPicker</a>
+          <Link to="/">ReactColorPicker</Link>
         </div>
         <div className="slider-container">
           <span>Level: {level}</span>
